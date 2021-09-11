19ae7e191883536a84c6a61a05af303a9163a465
AlexDyuba <adyuba@icloud.com>
2021-09-11 22:10:55 +0300

    create destr

---
+++ OOP_basics/result_test/task_2/Welcome.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Alex on 9/11/21.
+//
+
+#include "Welcome.h"
+#include <iostream>
+
+Welcome::Welcome()
+{
+    m_data = new char[14];
+    const char *init = "Hello, World!";
+    for (int i = 0; i < 14; ++i)
+        m_data[i] = init[i];
+}
+
+Welcome::~Welcome()
+{
+    delete[] m_data;
+}
+
+
+void Welcome::print() const
+{
+    std::cout << m_data;
+}
---
+++ OOP_basics/result_test/task_2/Welcome.h
@@ -0,0 +1,23 @@
+//
+// Created by Alex on 9/11/21.
+//
+
+#ifndef C__BSUIR_WELCOME_H
+#define C__BSUIR_WELCOME_H
+
+
+class Welcome {
+private:
+    char *m_data;
+
+public:
+    Welcome();
+
+    ~Welcome();
+
+    void print() const;
+
+};
+
+
+#endif //C__BSUIR_WELCOME_H
