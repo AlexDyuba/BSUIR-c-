89f21ea3ff3c8bfa9ef444b71f2537e5ffad9420
AlexDyuba <adyuba@icloud.com>
2021-09-16 23:03:07 +0300

    update

---
+++ operator_overloading_c++/lessson_139/String.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex on 9/14/21.
+//
+
+#include "String.h"
---
+++ operator_overloading_c++/lessson_139/String.h
@@ -0,0 +1,20 @@
+//
+// Created by Alex on 9/14/21.
+//
+
+#ifndef C__BSUIR_STRING_H
+#define C__BSUIR_STRING_H
+
+#include <string>
+
+using namespace std;
+
+
+class String {
+    string m_string;
+
+public:
+};
+
+
+#endif //C__BSUIR_STRING_H
